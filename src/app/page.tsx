83d74e537e80950411b87fc2dd36098f79a2e886
webmob <ltpg.webmob.dev@webmobsoft.com>
2025-08-02 12:34:28 +0530

    init-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div
-      className="min-h-screen bg-cover bg-center flex items-center justify-center px-6"
-      style={{
-        backgroundImage:
-          "url('https://img.freepik.com/free-vector/coming-soon-text-abstract-sunrise-dark-background-with-motion-effect_157027-1073.jpg')",
-      }}
-    >
-      <div className="bg-white/80 p-10 rounded-xl text-center shadow-lg backdrop-blur-md animate-fade-in space-y-6 max-w-xl">
-        <h1 className="text-4xl sm:text-5xl font-bold text-gray-900 tracking-tight animate-pulse">
+    <div className="flex items-center justify-center min-h-screen bg-dark px-6">
+      <div className="text-center space-y-6">
+        
+
+        <h1 className="text-5xl font-bold tracking-tight text-white-900">
           Coming Soon
         </h1>
-        <p className="text-gray-700 text-lg">
-          We're crafting something premium for golf lovers.
+        <p className="text-lg text-white-600">
+          We’re preparing something premium for golf lovers.
         </p>
-        <p className="text-sm text-gray-500">Stay tuned with LTP - Golf</p>
+
+        <div className="mt-6">
+          <p className="text-sm text-white-500">Stay tuned at LTP - Golf</p>
+        </div>
       </div>
     </div>
   );
