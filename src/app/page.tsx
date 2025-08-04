8495f9e78a617ab55982080cf1223c920ae6454f
webmob <ltpg.webmob.dev@webmobsoft.com>
2025-08-04 13:00:25 +0530

    init-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Coming Soon | LTPGGolf</title>
+        <title>Coming Soon | LTPG - Golf</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
